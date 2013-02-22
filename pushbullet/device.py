f4374d8c7bad3c209d13b33f136d7c9b0742ac26
Richard Borcsik <richard@borcsik.com>
2013-02-22 21:14:40 +0100

    Basic functionality working. Lists and Notes are supported.

---
+++ pushbullet/device.py
@@ -0,0 +1,51 @@
+import json
+
+class Device:
+
+	PUSH_URL = "https://www.pushbullet.com/api/pushes"
+
+	def __init__(self, session, device_info):
+		self._session = session
+		self.dev_id = device_info.get("id")
+		self.owner = device_info.get("owner_name", None)
+
+		extras = device_info["extras"]
+		self.model = extras.get("model")
+		self.manufacturer = extras.get("manufacturer")
+		self.android_version = extras.get("android_version")
+		self.sdk_version = extras.get("sdk_version")
+		self.app_version = extras.get("app_version")
+
+		self._fullname = "{} {} {}".format(self.manufacturer,
+										   self.model, self.android_version)
+		
+		nickname = extras.get("nickname")
+		self.name = nickname or self._fullname
+
+	def push_note(self, title, body):
+		data = {"type": "note", "title": title, "body": body}
+		return self._push(data)
+
+	def push_address(self, name, address):
+		pass
+
+	def push_list(self, title, items):
+		data = {"type": "list", "title": title, "items": items}
+		return self._push(data)		
+
+	def push_file(self, file):
+		pass
+
+	def push_link(self, title, url):
+		pass
+
+	def _push(self, data, headers={}):
+		data["device_id"] = self.dev_id
+		return self._session.post(self.PUSH_URL, data=json.dumps(data), headers=headers)
+
+	def __repr__(self):
+		return self.__str__()
+
+	def __str__(self):
+		owner_info = ", 'owner': '%s '}" % (self.owner) if self.owner else "}"
+		return "{'dev_id': %s, 'name': '%s'" % (self.dev_id, self.name) + owner_info
---
+++ pushbullet/pushbullet.py
@@ -0,0 +1,45 @@
+from itertools import chain
+
+import requests
+
+from .device import Device
+
+class PushBullet:
+
+    DEV_LIST_URL = "https://www.pushbullet.com/api/devices"
+
+    def __init__(self, apikey):
+        self._session = requests.Session()
+        self._session.auth = (apikey, "")
+        self._session.headers.update({"content-type": "application/json"})
+
+        self._devices = []
+        self._load_devices()
+
+    def _load_devices(self):
+        resp = self._session.get(self.DEV_LIST_URL)
+        resp_dict = resp.json()
+
+        own_devices = resp_dict.get("devices", [])
+        shared_devices = resp_dict.get("shared_devices", [])
+
+        devices = []
+        for device_info in chain(own_devices, shared_devices):
+            d = Device(self._session, device_info)
+            devices.append(d)
+
+        self._devices = devices
+
+    def reload_devices(self):
+        self._load_devices()
+
+    def devices(self):
+        return self._devices
+
+    def get(self, query):
+        if type(query) is int:
+            device = list(filter(lambda x: x.dev_id == query, self._devices))
+            if not device:
+                return None
+            else:
+                return device[0]
