d9c2db456c69760f325b771885508153ea8d0a3e
Richard Borcsik <borcsikrichard@gmail.com>
2014-06-09 22:05:07 +0200

    Work on V2 api compatibility. Device list and text pusing (except files) is working

--- pushbullet/device.py
+++ pushbullet/device.py
@@ -4,59 +4,43 @@ import requests
 
 class Device(object):
 
-	PUSH_URL = "https://api.pushbullet.com/api/pushes"
+	PUSH_URL = "https://api.pushbullet.com/v2/pushes"
 
-	def __init__(self, api_key, device_id, device_info = None):
+	def __init__(self, api_key, device_info):
 		self.api_key = api_key
-		self.device_id = device_id
+		self.device_iden = device_info.get("iden")
 
-		device_info = device_info or {}
-
-		self.owner = device_info.get("owner_name")
-
-		extras = device_info.get("extras", {})
-		self.model = extras.get("model")
-		self.manufacturer = extras.get("manufacturer")
-		self.android_version = extras.get("android_version")
-		self.sdk_version = extras.get("sdk_version")
-		self.app_version = extras.get("app_version")
-
-		self._fullname = "{} {} {}".format(self.manufacturer,
-										   self.model, self.android_version)
-
-		nickname = extras.get("nickname")
-		self.name = nickname or self._fullname
+		for attr in ("push_token", "app_version",
+					 "android_sdk_version", "fingerprint",
+					 "active", "nickname","manufacturer",
+					 "kind","created", "modified",
+					 "android_version", "model", "pushable"):
+			setattr(self, attr, device_info.get(attr))
 
 		self._json_header = {'Content-Type': 'application/json'}
 
 	def push_note(self, title, body):
 		data = {"type": "note", "title": title, "body": body}
-		return self._push(data, headers=self._json_header)
+		return self._push(data)
 
 	def push_address(self, name, address):
 		data = {"type": "address", "name": name, "address": address}
-		return self._push(data, headers=self._json_header)
+		return self._push(data)
 
 	def push_list(self, title, items):
 		data = {"type": "list", "title": title, "items": items}
-		return self._push(data, headers=self._json_header)
+		return self._push(data)
 
-	def push_file(self, file):
-		data = {"type": "file"}
-		files = {"file": file}
-		return self._push(data, files=files)
+	def push_link(self, title, url, body=None):
+		data = {"type": "link", "title": title, "url": url, "body": body}
+		return self._push(data)
 
-	def push_link(self, title, url):
-		data = {"type": "link", "title": title, "url": url}
-		return self._push(data, headers=self._json_header)
+	def _push(self, data):
+		data["device_iden"] = self.device_iden
 
-	def _push(self, data, headers={}, files = {}):
-		data["device_id"] = self.device_id
-		if not files:
-			data = json.dumps(data)
-		headers.update({"User-Agent": "ifttt2pushbullet.herokuapp.com"})
-		return requests.post(self.PUSH_URL, data=data, headers=headers,
-							 files=files, auth=(self.api_key, ""))
+		return requests.post(self.PUSH_URL, data=json.dumps(data),
+							 headers=self._json_header,
+							 auth=(self.api_key, ""))
 
-	def __repr__(self):
-		return "Device('{}', {})".format(self.api_key, self.device_id)
+	def __str__(self):
+		return self.nickname or (self.manufacturer + " " + self.model)
--- pushbullet/pushbullet.py
+++ pushbullet/pushbullet.py
@@ -12,45 +12,24 @@ else:
 
 class PushBullet(object):
 
-    DEV_LIST_URL = "https://api.pushbullet.com/api/devices"
+    DEVICES_ENDPOINT = "https://api.pushbullet.com/v2/devices"
 
     def __init__(self, api_key):
         self.api_key = api_key
 
-        self._devices = []
         self._load_devices()
 
     def _load_devices(self):
-        resp = requests.get(self.DEV_LIST_URL, auth=(self.api_key, ""))
+        self.devices = []
+
+        resp = requests.get(self.DEVICES_ENDPOINT, auth=(self.api_key, ""))
         resp_dict = resp.json()
 
-        own_devices = resp_dict.get("devices", [])
-        shared_devices = resp_dict.get("shared_devices", [])
+        device_list = resp_dict.get("devices", [])
 
-        devices = []
-        for device_info in chain(own_devices, shared_devices):
-            d = Device(self.api_key, device_info["id"], device_info)
-            devices.append(d)
+        for device_info in device_list:
+            d = Device(self.api_key, device_info)
+            self.devices.append(d)
 
-        self._devices = devices
-
-    def reload_devices(self):
+    def refresh(self):
         self._load_devices()
-
-    @property
-    def devices(self):
-        return self._devices
-
-    def get(self, query):
-        if isinstance(query, integer_type):
-            device = list(filter(lambda x: x.device_id == query, self._devices))
-            if not device:
-                return None
-            else:
-                return device[0]
-
-    def __getitem__(self, device_id):
-        if not isinstance(device_id, integer_type):
-            raise TypeError("device_id must be an integer")
-        else:
-            return self.get(device_id)
