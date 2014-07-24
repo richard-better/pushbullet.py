40a5bb14383b3a86f5c17ec08dd2a82248bc0db6
Igor Maculan <n3wtron@gmail.com>
2014-07-24 18:42:59 +0200

    created listener

---
+++ pushbullet/listener.py
@@ -0,0 +1,134 @@
+__author__ = 'Igor Maculan <n3wtron@gmail.com>'
+import logging
+import requests
+from threading import Thread
+import time
+import json
+
+import websocket
+
+
+log = logging.getLogger('pushbullet.Listener')
+
+WEBSOCKET_URL = 'wss://stream.pushbullet.com/websocket/'
+PUSHES_URL = 'https://api.pushbullet.com/v2/pushes'
+
+
+class PushTypeNotProvided(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+class Listener(Thread, websocket.WebSocketApp):
+    def __init__(self, api_key,
+                 device_id=None,
+                 on_note=None,
+                 on_link=None,
+                 on_address=None,
+                 on_list=None,
+                 on_file=None,
+                 http_proxy_host=None,
+                 http_proxy_port=None):
+        """
+        :param api_key: pushbullet Key
+        :param device_id: id of device to listen (if None listen all devices)
+        :param on_note: function like def on_note(pushed_note)
+        :param on_link: function like def on_link(pushed_link)
+        :param on_address: function like def on_address(pushed_address)
+        :param on_list: function like def on_list(pushed_list)
+        :param on_file: function like def on_file(pushed_file)
+        :param http_proxy_host: host proxy (ie localhost)
+        :param http_proxy_port: host port (ie 3128)
+        """
+        Thread.__init__(self)
+        websocket.WebSocketApp.__init__(self, WEBSOCKET_URL + api_key,
+                                        on_open=self.on_open,
+                                        on_message=self.on_message,
+                                        on_close=self.on_close)
+        self.api_key = api_key
+        self.device_id = device_id
+        self.connected = False
+        self.last_update = time.time()
+
+        # History
+        self.history = None
+        self.clean_history()
+        # specific pushes type methods
+        self.on_note = on_note
+        self.on_link = on_link
+        self.on_address = on_address
+        self.on_list = on_list
+        self.on_file = on_file
+
+        # proxy configuration
+        self.http_proxy_host = http_proxy_host
+        self.http_proxy_port = http_proxy_port
+        self.proxies = None
+        if http_proxy_port is not None and http_proxy_port is not None:
+            self.proxies = {
+                "http": "http://" + http_proxy_host + ":" + str(http_proxy_port),
+                "https": "http://" + http_proxy_host + ":" + str(http_proxy_port),
+            }
+
+    def clean_history(self):
+        self.history = dict(note=list(),
+                            link=list(),
+                            address=list(),
+                            list=list(),
+                            file=list())
+
+    def on_open(self, ws):
+        self.connected = True
+        self.last_update = time.time()
+
+    def on_close(self, ws):
+        log.debug('Listener closed')
+        self.connected = False
+
+    def on_message(self, ws, message):
+        log.debug('Message received:' + message)
+        try:
+            json_message = json.loads(message)
+            if json_message['type'] == 'tickle':
+                pushes_url = PUSHES_URL + '?modified_after=' + str(self.last_update)
+                logging.debug('calling:' + pushes_url)
+                res = requests.get(pushes_url, auth=(self.api_key, ""), proxies=self.proxies)
+                self.last_update = time.time()
+                pushes = res.json()['pushes']
+                for push in pushes:
+                    log.debug(str(push))
+                    if self.device_id is None or \
+                            ('target_device_iden' in push and \
+                                         push['target_device_iden'] == self.device_id):
+                        if push['type'] not in self.history.keys():
+                            raise PushTypeNotProvided('Type not provided:' + push['type'])
+                        else:
+                            # add the push to history
+                            self.history[push['type']].append(push)
+                            # call specific method/function
+                            if push['type'] == 'note' and self.on_note is not None:
+                                self.on_note(push)
+                            if push['type'] == 'link' and self.on_link is not None:
+                                self.on_link(push)
+                            if push['type'] == 'address' and self.on_address is not None:
+                                self.on_address(push)
+                            if push['type'] == 'list' and self.on_list is not None:
+                                self.on_list(push)
+                            if push['type'] == 'file' and self.on_file is not None:
+                                self.on_file(push)
+                log.debug(str(self.history))
+        except Exception as e:
+            logging.exception(e)
+
+    def run_forever(self, sockopt=None, sslopt=None, ping_interval=0, ping_timeout=None):
+        websocket.WebSocketApp.run_forever(self, sockopt=sockopt, sslopt=sslopt, ping_interval=ping_interval,
+                                           ping_timeout=ping_timeout,
+                                           http_proxy_host=self.http_proxy_host,
+                                           http_proxy_port=self.http_proxy_port)
+
+    def run(self):
+        self.run_forever()
+
