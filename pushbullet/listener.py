c1cd1cdd75f7bd80ebea81c3f9241a3555531aec
Richard Borcsik <borcsikrichard@gmail.com>
2014-07-28 16:50:10 +0200

    Remove device and push type check

--- pushbullet/listener.py
+++ pushbullet/listener.py
@@ -25,13 +25,11 @@ class PushTypeNotProvided(Exception):
 
 class Listener(Thread, websocket.WebSocketApp):
     def __init__(self, api_key,
-                 device_id=None,
                  on_push=None,
                  http_proxy_host=None,
                  http_proxy_port=None):
         """
         :param api_key: pushbullet Key
-        :param device_id: id of device to listen (if None listen all devices)
         :param on_push: function that get's called on all pushes
         :param http_proxy_host: host proxy (ie localhost)
         :param http_proxy_port: host port (ie 3128)
@@ -42,7 +40,6 @@ class Listener(Thread, websocket.WebSocketApp):
                                         on_message=self.on_message,
                                         on_close=self.on_close)
         self.api_key = api_key
-        self.device_id = device_id
         self.connected = False
         self.last_update = time.time()
 
@@ -61,11 +58,7 @@ class Listener(Thread, websocket.WebSocketApp):
             }
 
     def clean_history(self):
-        self.history = dict(note=list(),
-                            link=list(),
-                            address=list(),
-                            list=list(),
-                            file=list())
+        self.history = []
 
     def on_open(self, ws):
         self.connected = True
@@ -87,25 +80,9 @@ class Listener(Thread, websocket.WebSocketApp):
                 pushes = res.json()['pushes']
                 for push in pushes:
                     log.debug(str(push))
-                    if self.device_id is None or \
-                            ('target_device_iden' in push and \
-                                         push['target_device_iden'] == self.device_id):
-                        if push['type'] not in self.history.keys():
-                            raise PushTypeNotProvided('Type not provided:' + push['type'])
-                        else:
-                            # add the push to history
-                            self.history[push['type']].append(push)
-                            # call specific method/function
-                            if push['type'] == 'note' and self.on_note is not None:
-                                self.on_note(push)
-                            if push['type'] == 'link' and self.on_link is not None:
-                                self.on_link(push)
-                            if push['type'] == 'address' and self.on_address is not None:
-                                self.on_address(push)
-                            if push['type'] == 'list' and self.on_list is not None:
-                                self.on_list(push)
-                            if push['type'] == 'file' and self.on_file is not None:
-                                self.on_file(push)
+                    # add the push to history
+                    self.history.append(push)
+                    self.on_push(push)
                 log.debug(str(self.history))
         except Exception as e:
             logging.exception(e)
