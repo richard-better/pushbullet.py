3d9bd0a0e8c24855e3cb9fcd50ee0e34c3e31cd2
Christopher Flynn <flynnguy@gmail.com>
2015-04-23 08:11:45 -0400

    File uploads fail under python3 because _magic_get_file_type was returning a byte array which was failing. Calling .decode('utf-8') returns it as a unicode string.

--- pushbullet/filetype.py
+++ pushbullet/filetype.py
@@ -1,7 +1,7 @@
 def _magic_get_file_type(f, _):
     file_type = magic.from_buffer(f.read(1024), mime=True)
     f.seek(0)
-    return file_type
+    return file_type.decode('utf-8')
 
 
 def _guess_file_type(_, filename):
