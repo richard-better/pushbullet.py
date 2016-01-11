55d7ba6c571acc4379da3456a3478c1a842755ec
kovacsbalu <kovacsbalu@gmail.com>
2016-01-11 09:55:27 +0100

    test contact push

--- tests/test_contacts.py
+++ tests/test_contacts.py
@@ -1,19 +1,27 @@
 from __future__ import print_function
 
-import os
+import mock
+import time
 
-import pushbullet
+from pushbullet import contact
 
-API_KEY = os.environ["PUSHBULLET_API_KEY"]
 
 class TestContacts(object):
 
-    @classmethod
     def setup_class(cls):
-        cls.pb = pushbullet.Pushbullet(API_KEY)
+        cls.contact_email = "test.contact@example.com"
+        contact_info = {"active": True, "created": time.time(), "modified": time.time(), "name": "test contact", 
+                        "status": "user", "email": cls.contact_email, "email_normalized": "testcontact@example.com"}
+        cls.account = mock.Mock(return_value=True)
+        cls.contact = contact.Contact(cls.account, contact_info)
 
     def test_encoding_support(self):
-        for contact in self.pb.contacts:
-            # We're not actually intersted in the output, just that it doesn't
-            # cause any errors.
-            print(contact)
+        # We're not actually intersted in the output, just that it doesn't
+        # cause any errors.
+        print(self.contact)
+
+    def test_push(self):
+        data = {"title": "test title"}
+        self.contact._push(data)
+        pushed_data = {"title": "test title", "email": self.contact_email}
+        self.account._push.assert_called_with(pushed_data)
