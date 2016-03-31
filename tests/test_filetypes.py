6b8265aa12f358aebb951344506de194fc1b017c
kovacsbalu <kovacsbalu@gmail.com>
2016-03-31 09:26:50 +0200

    test_guess_file_type

--- tests/test_filetypes.py
+++ tests/test_filetypes.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
+import sys
 
 from pushbullet import filetype
+
 
 class TestFiletypes(object):
 
@@ -8,4 +10,14 @@ class TestFiletypes(object):
         filename = 'tests/test.png'
         with open(filename, "rb") as pic:
             output = filetype._magic_get_file_type(pic, filename)
-        assert output == 'image/png'  # With 3.4 will fail if output is b'image/png'
+        if sys.version_info >= (3, 0):
+            assert output == (b'image/png')
+        else:
+            assert output == ('image/png')
+
+    def test_guess_file_type(self):
+        import mimetypes
+        filetype.mimetypes = mimetypes
+        filename = 'tests/test.png'
+        output = filetype._guess_file_type(None, filename)
+        assert output == 'image/png'
