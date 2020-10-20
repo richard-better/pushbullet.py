9e92bc6209e9ecaf1ae1388cdb95eec918a2f91a
Richard Borcsik <git@richardb.me>
2020-10-20 21:30:54 +0200

    fix file type detection

--- pushbullet/filetype.py
+++ pushbullet/filetype.py
@@ -1,11 +1,17 @@
-def _magic_get_file_type(f, _):
-    file_type = magic_from_buffer(f.read(1024), mime=True)
-    f.seek(0)
-    return maybe_decode(file_type)
+
+try:
+    from magic import from_buffer as magic_from_buffer
+except ImportError:
+    import mimetypes
 
 
-def _guess_file_type(_, filename):
-    return mimetypes.guess_type(filename)[0]
+def get_file_type(file, filename):
+    try:
+        file_type = magic_from_buffer(file.read(1024), mime=True)
+        file.seek(0)
+        return maybe_decode(file_type)
+    except NameError:
+        return mimetypes.guess_type(filename)[0]
 
 
 # return str on python3.  Don't want to unconditionally
@@ -17,11 +23,3 @@ def maybe_decode(s):
         decoded = s
     return decoded
 
-
-try:
-    from magic import from_buffer as magic_from_buffer
-except ImportError:
-    import mimetypes
-    get_file_type = _guess_file_type
-else:
-    get_file_type = _magic_get_file_type
