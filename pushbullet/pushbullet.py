e88584420d402d6bf9cbb0d528e23e323e2a2d56
Patrick McMichael <patrick@patrickmcmichael.org>
2020-10-20 10:11:09 +0100

    Use curly brace literal instead of dict function (#135)

--- pushbullet/pushbullet.py
+++ pushbullet/pushbullet.py
@@ -106,7 +106,7 @@ class Pushbullet(object):
 
     @staticmethod
     def _recipient(device=None, chat=None, email=None, channel=None):
-        data = dict()
+        data = {}
         if device:
             data["device_iden"] = device.device_iden
         elif chat:
